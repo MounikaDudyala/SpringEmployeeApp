59eff3ce8646de994f420428f90de87ab5025911
MounikaDudyala <mounikadudyala1998@gmail.com.com>
2020-11-09 18:36:47 +0530

    for testing purpose of git from intellij

---
+++ src/main/java/com/mounika/springemployeeapp/modal/TestClass.java
@@ -0,0 +1,12 @@
+package com.mounika.springemployeeapp.modal;
+
+public class TestClass {
+    private String empId;
+    private String firstName;
+    private String lastName;
+    private String managerId;
+
+    public TestClass() {
+
+    }
+}
